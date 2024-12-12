8055fd28f3646c4bc5f79300b9f1a30af33fdf80
Tanishqsaini26 <127631886+Tanishqsaini26@users.noreply.github.com>
2024-12-12 15:06:02 +0530

    Add files via upload

---
+++ backend/controllers/expense.js
@@ -0,0 +1,39 @@
+const ExpenseSchema = require("../models/ExpenseModel")
+exports.addExpense = async(req, res) => {
+    const {title, amount, category, description, date} = req.body
+    const income = ExpenseSchema({
+        title,
+        amount,
+        category,
+        description,
+        date})
+    try {
+        if(!title || !category || !description || !date){
+            return res.statues(400).json({message: 'all fields are required!'})
+        }
+        if(amount <= 0 || !amount === 'number'){
+            return res.statues(400).json({message: 'amount must be a positive number!'})
+        }
+        await income.save()
+        res.status(200).json({message: 'Expense Added!'})
+    } catch (error) {
+        res.staus(500).json({message: 'Server Error!'})
+    }
+     console.log(income)}
+exports.getExpense = async(req, res) => {
+     try {
+        const incomes = await ExpenseSchema.find().sort({createdAt: -1})
+        res.status(200).json(incomes)
+        
+     } catch (error) {
+        res.status(500).json({message: 'Server Error!'})
+     }}
+exports.deleteExpense = async(req, res) => {
+   const {id} = req.params;
+   ExpenseSchema.findByIdAndDelete(id)
+   .then((income) =>{
+       res.status(200).json({message: 'Expense Deleted!'})
+   })
+   .catch((err) =>{
+    res.status(500).json({message: 'Server Error!'})
+   })}
